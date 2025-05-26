894db9e671fc1c60fb45f2a7f80234020a5854ab
mmdmuazu <muhammadaliyumuazu@gmail.com>
2025-05-26 18:33:16 -0400

    initial commit

--- PalmTrees/palm_trees/views.py
+++ PalmTrees/palm_trees/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .models import Product
+import json
 
 # Create your views here.
 def index(req):
     products = Product.objects.all()
     return render(req,'test.html',{'product':products})
-def register(req):
-    print(req)
+def register(req:json):
     if req.method == "POST":
-       print(req)
-       fullName:str =  req.POST.get("fullName")
-       print('This is your full name: ',fullName)
+       fullName =  user("",'fullName')
+       print('This is your full name: ',fullName,fullName)
     return render(req,'signup.html')
 
 def login(req):
     return render(req,'signin.html')
+
+def user(req,info:any) -> any:
+    try:
+        data = json.load(req)
+        information = data.get(info)
+        return information
+    except Exception as e:
+        return e
+    
